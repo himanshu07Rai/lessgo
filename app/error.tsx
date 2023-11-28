1f585d34a21e57672df875cfbdbe69a916940dbf
Himanshu Rai <raihimanshu22345@gmail.com>
2023-11-28 19:15:03 +0530

    navbar added

--- app/error.tsx
+++ app/error.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const error = ({ error, reset }: { error: Error; reset: () => void }) => (
-  <div className="grid h-screen px-4 bg-white place-content-center">
+  <div className="grid h-screen px-4 place-content-center">
     <h1 className="tracking-widest text-gray-500 uppercase">
       {error.name} | {error.message}
     </h1>
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,5 @@ import ProductCart from './components/ProductCart';
 import Users from './components/Users';
 
 export default function Home() {
-  return (
-    <main className="h-full">
-      <Link href="/users">users</Link>
-      <ProductCart />
-      <Users />
-    </main>
-  );
+  return <main className="h-full">Hello</main>;
 }
