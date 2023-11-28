26cb669e77493f38d00e79c4c0fc30074f91f2b7
Himanshu Rai <raihimanshu22345@gmail.com>
2023-11-28 20:49:18 +0530

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,16 +2,64 @@
 import React from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
-const page = () => (
-  <div>
-    <h1 className="text-3xl font-bold">Create Issue</h1>
-    <form className="flex flex-col space-y-4">
-      <input className="border border-gray-300 rounded-md p-2" placeholder="Title" />
-      <SimpleMDE className="border border-gray-300 rounded-md p-2" placeholder="Description" />
-      <button className="bg-blue-500 text-white rounded-md p-2">Create Issue</button>
-    </form>
-  </div>
-);
+enum Priority {
+  Low,
+  Medium,
+  High,
+}
+
+type IssueForm = {
+  title: string;
+  description: string;
+  priority: number;
+};
+
+const page = () => {
+  const router = useRouter();
+  const { register, handleSubmit, control } = useForm<IssueForm>();
+  const handleNewIssue = async (data: IssueForm) => {
+    try {
+      data.priority = parseInt(Priority[data.priority], 10);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Create Issue</h1>
+      <form className="flex flex-col space-y-4" onSubmit={handleSubmit(handleNewIssue)}>
+        <input
+          className="border border-gray-300 rounded-md p-2"
+          placeholder="Title"
+          {...register('title')}
+        />
+        <select className="border border-gray-300 rounded-md p-2" {...register('priority')}>
+          <option>Low</option>
+          <option>Medium</option>
+          <option>High</option>
+        </select>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE
+              className="border border-gray-300 rounded-md p-2"
+              placeholder="Description"
+              {...field}
+            />
+          )}
+        />
+        <button className="bg-blue-500 text-white rounded-md p-2">Create Issue</button>
+      </form>
+    </div>
+  );
+};
 
 export default page;
